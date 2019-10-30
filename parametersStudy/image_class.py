8ae4094715a8632713883921a5567a39af071b68
Kyle Farmer <kyle@Kyles-MacBook-Pro.local>
2019-10-30 18:27:20 -0500

    pushing sort of as a checkmark and begin a new part of the project. The study into finding good parameters and getting consistant text being read is hopefully coming to a close for a while. It was not a deep or statistically challenging study. The hope was to do something relatively basic but with a lot of data and just hope that the best images would group together in a smaller subset of parameters and that is exactly what happened. To get an ever more refined idea and parameter variance, a deeper study with more sophisticated statistical tools and techinques would have to be done. If runtime becomes a big problem then this will be revisited. Otherwise godspeed to the new task. ################################################################################################# I moved some files into directories to group them and to sort of get them out of the way. I dont think we will use these files and if we do then we should either alter the text to fit path or just pull everything out of their directories and run from the main OCR directory. Once again, godspeed.

---
+++ parametersStudy/image_class.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import cv2
+from PIL import Image
+import pytesseract
+
+
+class image_ocr:
+
+    def __init__(self):
+        self.properties = {'erode_kernel':None, 'erode_iter':None, 'dilate_kernel':None, 'dilate_iter':None,
+                           'width':None, 'blocksize':None, 'constant':None, 'thresh_type':None, 'scanned':False}
+        self.text = ''
+        self.image = None
+        self.string_count = 0
+        self.original_name = ''
+        self.words = []
+
+    def add_property(self, property, value):
+        if property in self.properties.keys():
+            self.properties[property] = value
+        else:
+            print('this is not a valid property')
+
+    def add_text(self):
+        if self.image is not None:
+            img = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+            im_pil = Image.fromarray(img)
+            im = im_pil
+            self.text = pytesseract.image_to_string(im)
+        else:
+            print('First add an image with .add_image(image)')
+
+    def add_image(self, image):
+        self.image = image
+
+    def add_string_count(self, count):
+        self.string_count = count
+
+    def add_name(self, name):
+        self.original_name = name
+
+    def add_words(self, words):
+        self.words = words
+
+
+
+
+if __name__ == '__main__':
+    image = cv2.imread(r'receipts/tjoes_1.jpg')
+    y = image_ocr()
+    y.add_image(image)
+    y.add_text()
+    print(y.text)
